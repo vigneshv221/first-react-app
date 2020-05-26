3168963b05066cc9abc02ef1120328be0ccc0a9f
vigneshv <vigneshv2211@gmail.com>
2020-05-26 22:12:51 +0530

    Started work on foodfinder react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React, { Component } from "react";
-import MainBox from "./components/box/MainBox";
+import Navbar from "./components/layout/Navbar";
 import "./App.css";
 
 export class App extends Component {
+    state = {
+        latitude: "",
+        longitude: "",
+    };
+
+    async componentDidMount() {
+        console.log("Getting location");
+        this.getLocation();
+    }
+
+    getLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                this.setState({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                });
+                console.log(
+                    "Latitude: " +
+                        this.state.latitude +
+                        "\nLongitude: " +
+                        this.state.longitude
+                );
+            });
+        }
+    };
+
     render() {
         return (
-            <div className='app'>
-                <MainBox />
+            <div style={{ marginTop: "0px", paddingTop: "0px" }}>
+                <Navbar />
+                <h1 className='mainbody heading'>Welcome to FoodFinder App</h1>
+                <p className='mainbody text'>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Eveniet doloremque earum possimus assumenda aperiam nam
+                    quibusdam nobis voluptate temporibus sint, vitae accusantium
+                    rem quo corrupti soluta natus sapiente. Hic, iure?
+                </p>
             </div>
         );
     }
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div className='navbar'>
+                <a href='/'>FOODFINDER</a>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
